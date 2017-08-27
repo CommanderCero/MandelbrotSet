4a348100956ad1fdc97bf8a5f4cdd6e02fef6e7f
Cero <dominikjeurissen@web.de>
2017-08-27 13:45:19 +0200

    Refactor: Increased Performance calculating the MandelbrotMap

--- MandelbrotSet/Model/MandelbrotMap.cs
+++ MandelbrotSet/Model/MandelbrotMap.cs
@@ -56,7 +56,7 @@ namespace MandelbrotSet.Model
             var newArr = new int[MapHeight][];
             for (var i = 0; i < newArr.Length; i++)
                 newArr[i] = new int[MapWidth];
-
+            
             //TODO Refactor Move
             #region dx
             if (pixeldx > 0)
@@ -67,10 +67,10 @@ namespace MandelbrotSet.Model
                 {
                     for (var x = MapWidth - pixeldx; x < MapWidth; x++)
                     {
-                        newArr[y][x] = GetIterations(new Complex(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY));
+                        newArr[y][x] = GetIterations(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY);
                     }
                 }
-
+            
                 //Copy
                 for (var y = 0; y < MapHeight; y++)
                 {
@@ -88,10 +88,10 @@ namespace MandelbrotSet.Model
                 {
                     for (var x = 0; x < pixeldx; x++)
                     {
-                        newArr[y][x] = GetIterations(new Complex(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY));
+                        newArr[y][x] = GetIterations(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY);
                     }
                 }
-
+            
                 //Copy
                 for (var y = 0; y < MapHeight; y++)
                 {
@@ -111,10 +111,10 @@ namespace MandelbrotSet.Model
                 {
                     for (var y = MapHeight - pixeldy; y < MapHeight; y++)
                     {
-                        newArr[y][x] = GetIterations(new Complex(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY));
+                        newArr[y][x] = GetIterations(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY);
                     }
                 }
-
+            
                 //Copy
                 for (var x = 0; x < MapWidth; x++)
                 {
@@ -132,10 +132,10 @@ namespace MandelbrotSet.Model
                 {
                     for (var y = 0; y < pixeldy; y++)
                     {
-                        newArr[y][x] = GetIterations(new Complex(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY));
+                        newArr[y][x] = GetIterations(RealStart + x * stepSizeX, ImaginaryStart + y * stepSizeY);
                     }
                 }
-
+            
                 //Copy
                 for (var x = 0; x < MapWidth; x++)
                 {
@@ -146,7 +146,7 @@ namespace MandelbrotSet.Model
                 }
             }
 
-            Map = newArr;
+            GenerateMap();
         }
 
         private void GenerateMap()
@@ -160,7 +160,7 @@ namespace MandelbrotSet.Model
                 arr[y] = new int[MapWidth];
                 for (var x = 0; x < MapWidth; x++)
                 {
-                    arr[y][x] = GetIterations(new Complex(RealStart + dx * x, ImaginaryStart + dy * y));
+                    arr[y][x] = GetIterations(RealStart + dx * x, ImaginaryStart + dy * y);
                 }
             }
 
@@ -169,13 +169,19 @@ namespace MandelbrotSet.Model
 
         //Calculates if the given c is bounded for the Mandelbrotformula
         //Zn+1 = Zn * Zn + c
-        private static int GetIterations(Complex c)
+        private static int GetIterations(double realC, double imagC)
         {
-            var z = Complex.Zero;
+            var real = 0.0;
+            var imag = 0.0;
             var n = 0;
-            while (z.Magnitude <= 2 && n < MaximumIterations)
+            //Magnitude(Zn) <= 2 The squareroot is performance heavy
+            //Magnitude(Zn)^2 <= 4 Same condition no squareroot
+            while (real * real + imag * imag <= 4 && n < MaximumIterations)
             {
-                z = z * z + c;
+                var newReal = real * real - imag * imag + realC;
+                var newImag = 2 * imag * real + imagC;
+                real = newReal;
+                imag = newImag;
                 n++;
             }
 
